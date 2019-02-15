33add2f7b28460a783308b19b45a0243033be8aa
TuanAnhPham <ptuananh196@gmail.com>
2019-02-15 21:27:18 +0700

    .

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
   rootPath:
-    process.env.REACT_APP_STATE === "localhost" ? "http://localhost:6969" : "https://techkidshotgirls.herokuapp.com"
+    process.env.REACT_APP_STATE === "localhost" ? "http://localhost:6969" : "https://tk-hotgirlserver.herokuapp.com"
 };
